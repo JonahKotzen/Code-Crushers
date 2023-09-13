d345b893cf8d8145fb9a6ab17a0be4d347ca5822
audreyb3380 <bergerau@bc.edu>
2023-09-13 15:47:49 -0400

    First commit

---
+++ Audrey_Berger_module2.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+pseed <- read_csv("pseed.fin.amps.csv")
+pseed.bl <- read_csv("pseed.lengths.csv")
+speeds <- read_csv("pseed.calibration.csv")
+
+pseed2 <- pseed%>%
+  left_join(speeds, by=c("speed"="vol"))%>%
+  print()
+view(pseed2)
+
+pseed2%>%
+  select(fish)%>%
+  unique()
+
+pseed2 <- pseed2%>%
+  left_join(pseed.bl,by="fish")
+
+pseed2 <- pseed2%>%
+  mutate(bl.s=cm.s/bl)%>%
+  print()
