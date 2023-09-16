246ee6257e420ce029c9c7240aaa8a05bf37b1f6
audreyb3380 <bergerau@bc.edu>
2023-09-15 20:06:26 -0400

    Continuing to work through the prompts

--- Audrey_Berger_module2.R
+++ Audrey_Berger_module2.R
@@ -1,96 +1,8 @@
-library(tidyverse)
-library(features)
-
-pseed <- read_csv("pseed.fin.amps.csv")
-pseed.bl <- read_csv("pseed.lengths.csv")
-speeds <- read_csv("pseed.calibration.csv")
-
-pseed2 <- pseed%>%
-  left_join(speeds, by=c("speed"="vol"))%>%
-  print()
-view(pseed2)
-
-pseed2%>%
-  select(fish)%>%
-  unique()
-
-pseed2 <- pseed2%>%
-  left_join(pseed.bl,by="fish")
-
-pseed2 <- pseed2%>%
-  mutate(bl.s=cm.s/bl)%>%
-  print()
-pseed2%>%
-  ggplot(aes(x=bl.s,y=amp.bl))+geom_point(alpha=0.01)
-pseed2%>%
-  filter(date=="2019-06-17-151149",fin=="L")%>%
-  ggplot(aes(x=frame,y=amp.bl))+geom_point()
-
-exp1 <- pseed2%>%
-  filter(date=="2019-06-17-151149",fin=="L")
-f1 <- features(x=exp1$frame,exp1$amp.bl)
-fget(f1)
-
-pseed2%>%
-  filter(date=="2019-06-17-151149",fin=="L")%>%
-  ggplot(aes(x=frame,y=amp.bl))+geom_point()+geom_vline(xintercept=fget(f1)$crit.pts)
-
-f2 <- features(x=exp1$frame,y=exp1$amp.bl*100)
-fget(f2)
-
-f.tib <- fget(f2)[2:3]%>%
-  as_tibble()%>%
-  filter(curvature<0)%>%
-  mutate(peaks=round(crit.pts,0))%>%
-  print()
-
-pseed2%>%
-  filter(date=="2019-06-17-151149", fin=="L")%>%
-  mutate(peak=frame %in% f.tib$peaks)%>%
-  ggplot(aes(x=frame,y=amp.bl,col=peak))+geom_point()
-
-find.peaks <- function(x,y,mult=100){
-  f <- fget(features(x=x,y=y*mult))[2:3]%>%
-    as_tibble()%>%
-    filter(curvature<0)%>%
-    mutate(peaks=round(crit.pts,0))
-  return(f$peaks)
-}
-
-pseed2%>%
-  filter(date%in%unique(date)[1:3])%>%
-  group_by(date,fin)%>%
-  mutate(peak=frame %in% find.peaks(frame,amp.bl))%>%
-  ggplot(aes(x=frame,y=amp.bl,alpha=peak,col=peak))+geom_point()+facet_grid(date~fin)
-
-pseed.max <- pseed2%>%
-  group_by(date,fin)%>%
-  mutate(peak=frame %in% find.peaks(frame,amp.bl))%>%
-  filter(peak==T)%>%
-  ggplot(aes(x=bl.s,y=amp.bl))+geom_point()+geom_smooth(method="lm")
-
-#this part is not working
-amp.aov <- aov(amp.bl~bl.s,pseed.max)
-summary(amp.aov)
-
-#telling me it can't apply to a ggplot
-pseed.max%>%
-  group_by(fish, bl.s)%>%
-  summarize(mean.max=mean(amp.bl))%>%
-  ggplot(aes(x=bl.s,y=mean.max,col=fish))+geom_point()+geom_smooth(method="lm")
-
-pseed.wide <- pseed2%>%
-  select(-amp)%>%
-  pivot_wider(names_from=fin, values_from=amp.bl)%>%
-  mutate(amp.sum=L+R)%>%
-  print()
-
-
-
 #this is where the code for the project report is
 library(tidyverse)
 library(features)
 
+#combine code to establish pseed.wide
 pseed <- read_csv("pseed.fin.amps.csv")
 pseed.bl <- read_csv("pseed.lengths.csv")
 speeds <- read_csv("pseed.calibration.csv")
@@ -108,15 +20,25 @@ pseed.wide <- pseed2 %>%
   view()
 
 #Create a custom function that computes SE
-se_mean <- function(x){
-  se <- sd(x)/sqrt(length(x))
-  return(se)
+
+#x and y values, amplitude and timeframe, pass info to features function
+#will only take peaks for own tibble, pass amplitude data and summarize
+#group by fish, experiment, fin
+#use values of those arguments in brackets to do operations on those values
+se_mean <- function(amp.sum){
+  se <- sd(amp.sum)/sqrt(length(amp.sum))
 }
 
 pseed.sum.max <- pseed.wide%>%
   group_by(fish,speed)%>%
-  mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
-  filter(peak==T)%>%
-  se_mean()
+  summarize(
+    amp.sum.mean=mean(amp.sum,na.rm=TRUE),
+    amp.sum.se=se_mean(amp.sum))
 
-  
+view(pseed.sum.max)
+#need to find the peaks since these are the max amps
+#amp.sum=amp of both fins, group_by(fish, speed)
+#will have to compute amplitude of cycles in each experiment
+#6 look for common variable names, look at table
+  #fish, date, speeds and body lengths are common variable
+#use features to fnd max of amp.sums
