73ac712004c0f61494363a913236ba515557b25c
Lauren Geary <laurengeary@Laurens-MacBook-Pro-8.local>
2023-09-17 21:14:10 -0400

    Lauren Geary Module 2 Project

--- Lauren_Geary_Module2.R
+++ Lauren_Geary_Module2.R
@@ -26,18 +26,30 @@ compute_se<-function(x){
   return(se)
 }
 
-#3-#4: Compute the mean maximum of all amp.sums across all fin-beat cycles
-pseed.sum.max <- pseed.wide%>%
-  group_by(fish,speed,bl.s)%>%
-  summarize(amp.sum.mean=mean(amp.sum,na.rm=TRUE),
-    amp.sum.se=compute_se(amp.sum))%>%
-  print()
+#3) Determine the maxes and then find the mean max of amp.sum values
+find.maxes <- function(x,y,mult=100){
+  f <- fget(features(x=x,y=y*mult))[2:3]%>%
+    as_tibble()%>%
+    filter(curvature<0)%>%
+    mutate(peak=round(crit.pts,0))
+  return(f$peak)
+}
+pseed.peaks <- pseed.wide%>%
+  group_by(date,fish)%>%
+  mutate(peak=frame%in% find.maxes(frame,amp.sum))%>%
+  filter(peak==T)
 
-#5: Plot the mean amp.sum vs. specific swimming speed and add error bars that correspond to the SE of amp.sum
-ggplot(pseed.sum.max, aes(x = speed, y = amp.sum.mean, color = fish)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = amp.sum.mean - amp.sum.se, ymax = amp.sum.mean + amp.sum.se), width = 0.2) +
-  labs(x = "Specific Swimming Speed", y = "Mean amp.sum", title = "Mean amp.sum vs. Swimming Speed")
+#4) Add the amp.sum.se values (this is also the code that adds amp.sum.max)
+pseed.sum.max <- pseed.peaks%>%
+  group_by(fish,speed,bl.s)%>%
+  summarize(
+    amp.sum.mean=mean(amp.sum,na.rm=TRUE),
+    amp.sum.se=se_mean(amp.sum))
+
+#5) Plot the mean amp.sum vs specific swimming speed with error bars
+ggplot(pseed.sum.max,aes(x=bl.s,y=amp.sum.mean,col=fish)) +
+  geom_errorbar(aes(ymin=amp.sum.mean-amp.sum.se, ymax=amp.sum.mean+amp.sum.se), width=.1) +geom_point() +
+  labs(x = "Specific Swimming Speed", y="Mean amp.sum", title = "Mean amp.sum vs. Swimming Speed")
 
 #6: Download pseed.met.rate and merge with new pseed.sum.max tibble
 pseed.met.rate<-read_csv("pseed.met.rate.csv")
@@ -48,7 +60,7 @@ pseed.final <- left_join(pseed.sum.max, pseed.met.rate, by="bl.s")%>%
 view(pseed.final)
 
 #7: Plot metabolic power output vs. mean maximum of amp.sum
-ggplot(merged_data, aes(x = amp.sum.mean, y = met.rate, color = fish)) +
-  geom_point() +
+pseed.met.rate2%>%
+  ggplot(aes(x=amp.sum.mean, y=met.rate, colour = fish))+geom_point()
   labs(x = "Mean amp.sum", y = "Metabolic Power Output", title = "Metabolic Power vs. Mean amp.sum")
 
